88d080cc494b8ded3b3df5743d574280f4f76450
charles.washington <charles.washington@crunchyroll.com>
2025-11-11 17:58:04 -0800

    Added additional dependencies

--- AniMusicPlayer/app/build.gradle.kts
+++ AniMusicPlayer/app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -54,6 +57,9 @@ dependencies {
     implementation(libs.bundles.media3)
     implementation(libs.coil)
     implementation(libs.coil.compose)
+    implementation(libs.bundles.ktor)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
